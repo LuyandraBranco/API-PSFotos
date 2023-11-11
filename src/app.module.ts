966b50bd11de95d6abc4dd6562b8e8e9c8ce2821
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2023-11-11 03:14:35 +0100

    src: serviço de autenticação da Google adicionado na aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthGoogleModule } from './auth-google/auth-google.module';
 
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, AuthGoogleModule],
   controllers: [],
   providers: [],
 })
---
+++ src/auth-google/google.stategy.ts
@@ -0,0 +1,31 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GoogleStategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: '64041320105-5b1e18ujms0n687h0tg8bh16nkcvjf4f.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      scope: ['email','profile'],
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const {name, emails,photos}= profile;
+    const user = {
+        email: emails[0].value,
+        firstName: name.givenName,
+        lastName: name.familyName,
+        pictures: photos[0].value,
+        accessToken
+    }
+    done(null,user);
+  }
+}
